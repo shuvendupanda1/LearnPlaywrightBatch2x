4cd47d8e46eb9b73fdfd62fc5b2ff5a7caf4db82
shuvendupanda1 <shuvendupanda1@users.noreply.github.com>
2026-05-06 20:09:36 +0530

    Initial commit: Add LearnPlaywrightBatch2x project files

---
+++ chapter_01_Basic/01_Basics.js
@@ -0,0 +1,6 @@
+console.log("Hello, World!");
+console.log("This is a simple JavaScript program.");
+console.log("Hello testing academy");
+
+let x = 10;
+console.log(x);
---
+++ chapter_01_Basic/04_Hotcode.js
@@ -0,0 +1,4 @@
+// This is a "hot" loop because it repeats the code inside 100 times
+for (let i = 0; i < 100; i++) {
+    console.log("Running iteration number: " + i);
+}
