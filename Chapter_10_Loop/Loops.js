6ae121576574df975aac6c169942ad8591481276
shuvendupanda1 <shuvendupanda1@users.noreply.github.com>
2026-06-22 17:48:59 +0530

    Add Chapter 10 (Loops) with for, while, do-while, for-of/in examples and interview questions

---
+++ Chapter_10_Loop/Loops.js
@@ -0,0 +1,46 @@
+
+
+/*for (statement 1; statement 2; statement 3) {
+    // Code here . . .
+}
+*/    
+
+
+for (let i = 0; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log("Even number"+ i);
+  }
+};
+
+/*
+
+while (condition) {
+    // code block
+}
+
+*/
+
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+
+//Do while loop
+
+/*
+do {
+    // code block
+} while (condition);
+ 
+*/
+
+let i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i <= 5);
+
+
